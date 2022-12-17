14fab43d85e3fcabc3a5d2db0fca379d6d344d8e
Your Name <you@example.com>
2022-12-17 20:32:25 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,13 @@ public class Main {
         obj.meth();
     }
 }
+class A {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
